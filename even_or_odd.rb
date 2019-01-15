3880ed428892091bbefc9fbf77c6d9db62471039
luis chagas <luischagas78@gmail.com>
2019-01-15 10:20:03 -0500

    created simple even or odd but it is still missing the question if you want to write another number or if it is enough and quit program

--- even_or_odd.rb
+++ even_or_odd.rb
@@ -1,7 +1,59 @@
-# Even or odd?
+# def number? string
+#   true if Float(string) rescue false
+# end
+
+# # Even or odd?
+
+# puts "Do you want to know if a number is even or odd?"
+# user_first_choice = gets.chomp
+
+# loop do
+#   if user_first_choice == "yes"
+#     puts "Write a number please."
+#     user_second_choice = gets.chomp.to_i
+# #    loop do
+#     if number?(user_second_choice)
+#       if user_second_choice.even?
+#         puts "It's even!"
+#       else user_second_choice.odd?
+#         puts "It's odd!"
+#       end
+#     else
+#       puts "Incorrect input! Please write a number."
+#       user_second_choice = gets.chomp.to_i
+#     end
+# #    end
+#   elsif user_first_choice == "no"
+#     puts "Ok. Bye Bye!"
+#     break
+#   else
+#     puts "Please just write yes or no."
+#     user_first_choice = gets.chomp
+#   end
+# end
 
 puts "Do you want to know if a number is even or odd?"
+user_first_choice = gets.chomp
 
-sleep(2)
-
-puts "Ok, write a number please."
+loop do
+  if user_first_choice == "yes"
+    puts "Write a number please."
+    user_second_choice = gets.chomp
+    if user_second_choice =~ /\A[-+]?\d*\.?\d+\z/
+      user_second_choice = user_second_choice.to_i
+      if user_second_choice%2 == 0
+        puts "It's even!"
+      else user_second_choice%2 != 0
+        puts "It's odd!"
+      end
+    else
+      puts "Incorrect input!"
+    end
+  elsif user_first_choice == "no"
+    puts "Ok. Bye Bye!"
+    break
+  else
+    puts "Please just write yes or no."
+    user_first_choice = gets.chomp
+  end
+end
