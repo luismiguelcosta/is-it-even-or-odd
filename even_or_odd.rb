f017b46c30460a0984e94fcc69eee20c34e15007
luis chagas <luischagas78@gmail.com>
2019-01-16 05:26:24 -0500

    not finished, still fighting with some iteration problems

--- even_or_odd.rb
+++ even_or_odd.rb
@@ -32,23 +32,110 @@
 #   end
 # end
 
+# puts "Do you want to know if a number is even or odd?"
+# user_first_choice = gets.chomp
+
+# loop do
+
+#   if user_first_choice == "yes"
+#     puts "Write a number please."
+#     user_second_choice = gets.chomp
+
+#     if user_second_choice =~ /\A[-+]?\d*\.?\d+\z/
+#       user_second_choice = user_second_choice.to_i
+
+#       if user_second_choice%2 == 0
+#         puts "It's even!"
+#         sleep(1)
+#         puts "Do you want to write another number?"
+#         user_first_choice = gets.chomp
+
+#         loop do
+
+#           if user_first_choice == "yes"
+#             puts "Write a number please."
+#             user_second_choice = gets.chomp
+#           elsif user_first_choice == "no"
+#             puts "Ok. Bye Bye!"
+#             break
+#           else
+#             puts "Please just write yes or no."
+#             user_first_choice = gets.chomp
+#           end
+
+#         end
+
+#       else user_second_choice%2 != 0
+#         puts "It's odd!"
+#       end
+
+#     else
+#       puts "Incorrect input!"
+#     end
+
+#   elsif user_first_choice == "no"
+#     puts "Ok. Bye Bye!"
+#     break
+#   else
+#     puts "Please just write yes or no."
+#     user_first_choice = gets.chomp
+#   end
+
+# end
+
+# user_second_choice = 3
+
+# def evenodd
+#   if user_second_choice.even?
+#     return "It's even!"
+#   else
+#     return "It's odd!"
+#   end
+# end
+
+# puts user_second_choice.even?
+
 puts "Do you want to know if a number is even or odd?"
 user_first_choice = gets.chomp
 
 loop do
+
   if user_first_choice == "yes"
     puts "Write a number please."
     user_second_choice = gets.chomp
+
     if user_second_choice =~ /\A[-+]?\d*\.?\d+\z/
       user_second_choice = user_second_choice.to_i
-      if user_second_choice%2 == 0
+
+      if user_second_choice.even?
         puts "It's even!"
-      else user_second_choice%2 != 0
+        # sleep(1)
+        puts "Do you want to try another number?"
+        user_first_choice = gets.chomp
+
+        loop do
+
+          if user_first_choice == "yes"
+            puts "Write a number please."
+            user_second_choice = gets.chomp
+          elsif user_first_choice == "no"
+            puts "Ok. Bye Bye!"
+            break
+          else
+            puts "Please just write yes or no."
+            user_first_choice = gets.chomp
+          end
+
+        end
+
+      else user_second_choice.odd?
         puts "It's odd!"
       end
+
     else
       puts "Incorrect input!"
     end
+
   elsif user_first_choice == "no"
     puts "Ok. Bye Bye!"
     break
@@ -56,4 +143,5 @@ loop do
     puts "Please just write yes or no."
     user_first_choice = gets.chomp
   end
+
 end
