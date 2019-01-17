854565cadb9828bc3ecfcfa3e6d40249ded2daff
luis chagas <luischagas78@gmail.com>
2019-01-17 04:37:48 -0500

    now it al works as I wanted, I'm satisfied

--- even_or_odd.rb
+++ even_or_odd.rb
@@ -1,31 +1,68 @@
+# loop do
 
-puts "Do you want to know if a number is even or odd?"
-user_first_choice = gets.chomp
+#   puts "Do you want to know if a number is even or odd?"
+#   user_first_choice = gets.chomp
 
-if user_first_choice.downcase == "no"
-  puts "Ok."
-else
-  puts "Incorrect input."
-end
+#   if user_first_choice.downcase == "no"
+#     puts "Ok."
+#     break
+#   else
+#     puts "Incorrect input."
+#   end
 
-if user_first_choice.downcase == "yes"
+#   if user_first_choice.downcase == "yes"
 
-  puts "Write a number please."
-  user_second_choice = gets.chomp
+#     puts "Write a number please."
+#     user_second_choice = gets.chomp
 
-  if user_second_choice =~ /\A[-+]?\d*\.?\d+\z/
+#     if user_second_choice =~ /\A[-+]?\d*\.?\d+\z/
 
-    user_second_choice = user_second_choice.to_i
+#       user_second_choice = user_second_choice.to_i
 
-    if user_second_choice.even?
-      puts "It's even!"
-    else
-      puts "It's odd!"
+#       if user_second_choice.even?
+#         puts "It's even!"
+#       else
+#         puts "It's odd!"
+#       end
+
+#     end
+
+#   end
+
+# end
+
+# puts "Bye Bye!"
+
+
+loop do
+
+  puts "Do you want to know if a number is even or odd?"
+  user_first_choice = gets.chomp
+
+  if user_first_choice.downcase == "no"
+    puts "Ok."
+    break
+
+  elsif user_first_choice.downcase == "yes"
+    puts "Write a number please."
+    user_second_choice = gets.chomp
+
+    if user_second_choice =~ /\A[-+]?\d*\.?\d+\z/
+
+      user_second_choice = user_second_choice.to_i
+
+      if user_second_choice.even?
+        puts "It's even!"
+      else
+        puts "It's odd!"
+      end
+
     end
 
+  else
+    puts "Incorrect input."
   end
 
 end
 
 puts "Bye Bye!"
-
